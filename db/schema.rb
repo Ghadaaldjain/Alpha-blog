048940cc9cc15a47c399e9785c72298efc4ad584
ghadaaldjain <ghadaaldjain@gmail.com>
2020-02-29 00:07:21 +0300

    User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_153813) do
+ActiveRecord::Schema.define(version: 2020_02_28_205947) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
 end
